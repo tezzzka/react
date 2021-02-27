80c4b7095ba51d605e839d7791ce385edf30a573
sur <apstaver@gmail.com>
2021-02-27 14:48:16 +0500

    webpack configured

--- hm01/src/index.js
+++ hm01/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    //return React.createElement('a',{id: 'test-id', className:'linknae'},'Hello from React');
+    return <div id='test-id'>Hello from React</div>
+}
+//ReactDOM.render(React.createElement(App),document.querySelector('#root'));
+ReactDOM.render(<App />,document.querySelector('#root'));
--- hm01/webpack.config.js
+++ hm01/webpack.config.js
@@ -9,5 +9,18 @@ module.exports = {
         path: path.resolve(__dirname,'static','build'),
         filename: 'bundle.js',
         publicPath: '/static/build/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname,'src'),
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/env','@babel/preset-react']
+                }
+            }
+        ]
     }
 }
